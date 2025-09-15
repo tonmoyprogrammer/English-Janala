5fa1f8b23a7d38dfa6f70e5375cd0261381af0b9
tonmoy programmer <debtonmoy583@gmail.com>
2025-09-15 16:20:45 +0600

    this is my third commit with you as a developer

--- janala.js
+++ janala.js
@@ -1,106 +1,62 @@
 
-// const learnLesson = ()=>
-// {
-//     fetch("https://openapi.programming-hero.com/api/levels/all")
-//     .then(res => res.json())
-//     .then(data => getLesson(data.data));
-// }
-
-
-// function buttonClick(id)
-// {
-//     const url = `https://openapi.programming-hero.com/api/level/${id}`
-//    fetch(url)
-//    .then( res => res.json())
-//     .then(data => getWord(data.data))
-// }
-
-
-// // {id: 19, level: 1, word: 'Sincere', meaning: 'সত্‍ / আন্তরিক', pronunciation: 'সিনসিয়ার'}
-
-// const getWord = (words) =>
-// {
-//     const mainContainer = document.getElementById("word-container");
-//     mainContainer.innerHTML = "";
-    
-//   for(let word of words)
-//   {
-    
-//       const newDiv =  document.createElement("div");
-//     newDiv.innerHTML = `
-//       <div class="bg-white rounded-xl shadow-sm py-15 text-center space-y-4" >
-
-            // <h2 class="font-bold text-2xl">${word.word}</h2>
-            // <p class="my-3">Meaning /Pronounciation</p>
-            // <h2 class="fon-bangla font-semibold text-2xl text-gray-700">${word.meaning} /${word.pronunciation}</h2>
-
-            // <div class="flex justify-between m-5">
-
-            //     <buttion class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></buttion>
-            //     <buttion class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></buttion>
-
-            // </div>
-//         </div>
-
-
-//     `
-//     mainContainer.appendChild(newDiv);
-//   }
-// }
-
-
-
-
-// const getLesson = (arrayOfData)=>
-// {
-
-//     const mainDiv = document.getElementById("level-container");
-//     mainDiv.innerHTML="";
-
-//     arrayOfData.forEach((value)=>
-//     {
-//         const newElement = document.createElement("div");
-//         newElement.innerHTML = `
-        
-//                  <button onclick = "buttonClick(${value.level_no}) "class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open-reader"></i>Learn - ${value.level_no}</button>
-//         `
-//         mainDiv.appendChild(newElement);
-//     });
-    
-
-// }
-// learnLesson();
-
 function learnLesson()
 {
     const url = "https://openapi.programming-hero.com/api/levels/all"
     fetch(url)
     .then(res => res.json())
     .then(data => getLesson (data.data));
+}
 
-    "level-container"
+    
+
+const removeActive = ()=>
+{
+    const  activeData = document.querySelectorAll(".lesson-btn");
+    activeData.forEach(data => data.classList.remove("active"));
 }
 function buttonClick(id)
 {
     const url = `https://openapi.programming-hero.com/api/level/${id}`
     fetch(url)
     .then(res => res.json())
-    .then(data => getWord(data.data));
+    .then((data)=>
+    {
+        removeActive();
+        const lessonClick = document.getElementById(`lesson-bt-${id}`);
+        lessonClick.classList.add("active");
+        getWord(data);
+    })
 }
+
+
 
 const getWord = (word)=>
 {
-    console.log(word);
-
     const mainDiv = document.getElementById("word-container");
      mainDiv.innerHTML ="";
+
+    if(word.length == 0)
+    {
+        mainDiv.innerHTML = `
+        
+        <div class="word-child-container text-center bg-slate-200 col-span-full py-10 space-y-3 font-bangla rounded-lg shadow-sm">
+            <img src="assets/alert-error.png" alt="" class=" mx-auto ">
+
+            <p class="text-gray-500">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h1 class="font-semibold text-4xl">নেক্সট Lesson এ যান </h1>
+
+        </div>
+ `
+    }
     word.forEach(element => {
         const newWord = document.createElement("div");
         newWord.innerHTML = `
           <div class="bg-white rounded-md shadow-sm text-center py-15">
-                         <h2 class="font-bold text-2xl">${element.word}</h2>
+                         <h2 class="font-bold text-2xl">${element.word ? element.word : "কোনো শব্দ যুক্ত করা হয়নি"}</h2>
             <p class="my-3">Meaning /Pronounciation</p>
-            <h2 class="fon-bangla font-semibold text-2xl text-gray-700">${element.meaning} /${element.pronunciation}</h2>
+            <h2 class="fon-bangla font-semibold text-2xl text-gray-700">
+            ${element.meaning ? element.meaning : "কোনো শব্দ যুক্ত করা হয়নি"} /${element.pronunciation ?
+                 element.pronunciation : "কোনো  Pronunciation যুক্ত করা হয়নি"}</h2>
 
             <div class="flex justify-between m-5">
                 <buttion class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></buttion>
@@ -115,7 +71,7 @@ const getWord = (word)=>
         
     });
     
-}
+    }
 
 const getLesson = (lesson)=>
 {
@@ -128,11 +84,12 @@ const getLesson = (lesson)=>
         const newElement = document.createElement("div");
         newElement.innerHTML=
         `
-        <button onclick = "buttonClick(${leson.level_no})" class="btn btn-outline btn-primary">
+        <button id= 'lesson-btn-${leson.level_no}' onclick = "buttonClick(${leson.level_no})" class="btn btn-outline btn-primary lesson-btn">
         <i class="fa-solid fa-book-open-reader"></i> Lesson-${leson.level_no}</button>
 
         `
         mainContainer.appendChild(newElement);
     }
 }
+
 learnLesson();
